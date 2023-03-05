3e5eff67227f379bdf2219ca6d681121841c6502
JayJay1024 <lygm1062@gmail.com>
2023-03-05 16:52:46 +0800

    chore: mui working with tailwind-css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,36 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
-
+import { StyledEngineProvider, ThemeProvider, createTheme } from "@mui/material/styles";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+const rootElement = document.getElementById("__next");
+const theme = createTheme({
+  components: {
+    MuiPopover: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+    MuiPopper: {
+      defaultProps: {
+        container: rootElement,
+      },
+    },
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </StyledEngineProvider>
       <Analytics />
     </>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
+import Button from "@mui/material/Button";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -15,6 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <Button variant="contained">Hello World</Button>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
