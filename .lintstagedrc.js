daf05ad080cd08e2863fadee3a7ddfb44af40668
JayJay1024 <lygm1062@gmail.com>
2023-03-05 13:43:49 +0800

    chore: fix lint and setup vercel analytics

---
+++ .lintstagedrc.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
+
+module.exports = {
+  "src/**/*.css": "stylelint --fix",
+  "src/**/*.{js,jsx,ts,tsx}": [buildEslintCommand],
+};
