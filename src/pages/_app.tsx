3c108e22b39f7d85d6e17e8df5902f6ce6a6ce98
JayJay1024 <lygm1062@gmail.com>
2023-03-06 22:19:38 +0800

    chore: app head config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { useEffect, useState } from "react";
 import { StyledEngineProvider, ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import Head from "next/head";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -35,6 +36,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <StyledEngineProvider injectFirst>
+        <Head>
+          <title>Bow Wow</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="Bow wow" />
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Component {...pageProps} />
