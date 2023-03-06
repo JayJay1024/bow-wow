91c2ac4bceb08ac27669c45df2c5e92651a7a135
JayJay1024 <lygm1062@gmail.com>
2023-03-06 19:59:38 +0800

    fix: document undefined

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,36 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { useEffect, useState } from "react";
 import { StyledEngineProvider, ThemeProvider, createTheme } from "@mui/material/styles";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-const rootElement = document.getElementById("__next");
-const theme = createTheme({
-  components: {
-    MuiPopover: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-    MuiPopper: {
-      defaultProps: {
-        container: rootElement,
-      },
-    },
-  },
-});
-
 export default function App({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = useState(createTheme());
+
+  useEffect(() => {
+    const rootElement = document.getElementById("__next");
+    setTheme(
+      createTheme({
+        components: {
+          MuiPopover: {
+            defaultProps: {
+              container: rootElement,
+            },
+          },
+          MuiPopper: {
+            defaultProps: {
+              container: rootElement,
+            },
+          },
+        },
+      })
+    );
+  }, []);
+
   return (
     <>
       <StyledEngineProvider injectFirst>
