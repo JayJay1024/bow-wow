942ddef0f045f9892696beebc239e95eab634008
JayJay1024 <lygm1062@gmail.com>
2023-04-12 12:36:44 +0800

    fix: monthly project

--- src/data/monthly.ts
+++ src/data/monthly.ts
@@ -7,10 +7,10 @@ export const monthlyData: MonthlyData[] = [
   },
   {
     day: 12,
-    projects: ["JB8888", "WSD8888", "HB8888"],
+    projects: ["JB8888", "WSD8888"],
   },
   {
     day: 20,
-    projects: ["SDD8888"],
+    projects: ["SDD8888", "HB8888"],
   },
 ];
